b26a388986788c7a8d04ff94703c81d7b641f06b
Dustin Zook <dustin.w.zook@gmail.com>
2022-09-16 14:34:48 -0500

    completed is even or odd loop

--- js/for_loops.js
+++ js/for_loops.js
@@ -9,7 +9,19 @@
         }
         return "The multiplication table is for " + input;
     }
-    alert(showMultiplicationTable(7));
+    //alert(showMultiplicationTable(7));
+
+
+    for(let i = 0; i < 10; i++){
+        let randomNum = Math.floor(Math.random() * (200 - 20 +1)+ 20);
+        if(randomNum % 2 === 0){
+            console.log(randomNum + " is even");
+        }else {
+            console.log(randomNum + " is odd");
+        }
+    }
+
+
 
 
 
