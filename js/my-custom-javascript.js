20014de5dadfb137a43ceff33b58193a1506a118
Dustin Zook <dustin.w.zook@gmail.com>
2022-10-19 15:35:25 -0500

    completed selectors exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,5 +1,30 @@
 "use strict";
 
 $(function(){
-    alert("the DOM has finished loading!")
+    // alert("the DOM has finished loading!")
+    //
+    // // id selectors
+    let mainHeader = $('#main-container').html();
+    alert(mainHeader);
+    let firstList = $('#first-list').html();
+    alert(firstList);
+    //
+    // // class selectors
+    $('.codeup').css('border', '1px solid red');
+    //
+    // //element selectors
+    $('li').css('font-size', '20px')
+    //
+    let statement = $('h1').html();
+    alert(statement);
+    //
+    // //multiple selectors
+    $('h1,p,li').css('background-color', 'yellow')
+    //
+    // //all selector
+    $('*').css('color', 'red')
+
+
 })
+
+
