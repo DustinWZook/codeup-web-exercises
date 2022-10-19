f6d6b1128f578c181897248df91a376eed37029d
Dustin Zook <dustin.w.zook@gmail.com>
2022-10-19 15:37:14 -0500

    completed event jquery exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -4,27 +4,33 @@ $(function(){
     // alert("the DOM has finished loading!")
     //
     // // id selectors
-    let mainHeader = $('#main-container').html();
-    alert(mainHeader);
-    let firstList = $('#first-list').html();
-    alert(firstList);
+    // let mainHeader = $('#main-container').html();
+    // alert(mainHeader);
+    // let firstList = $('#first-list').html();
+    // alert(firstList);
     //
     // // class selectors
-    $('.codeup').css('border', '1px solid red');
+    // $('.codeup').css('border', '1px solid red');
     //
     // //element selectors
-    $('li').css('font-size', '20px')
+    // $('li').css('font-size', '20px')
     //
-    let statement = $('h1').html();
-    alert(statement);
+    // let statement = $('h1').html();
+    // alert(statement);
     //
     // //multiple selectors
-    $('h1,p,li').css('background-color', 'yellow')
+    // $('h1,p,li').css('background-color', 'yellow')
     //
     // //all selector
-    $('*').css('color', 'red')
+    // $('*').css('color', 'red')
 
+    $('h1').click(function(){
+        $(this).css('background-color', 'blue');
+    })
 
+    $('p').dblclick(function (){
+        $(this).css('font-size', '18px')
+    })
 })
 
 
